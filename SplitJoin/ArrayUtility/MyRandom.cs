3274a72dd1ce969522fd03b5bcd7ded324b97d62
John Kenedy <sorainnosia@gmail.com>
2021-12-14 13:46:25 +0800

    Add files via upload

---
+++ SplitJoin/ArrayUtility/MyRandom.cs
@@ -0,0 +1,17 @@
+﻿namespace ArrayUtility
+{
+    public class MyRandom
+    {
+        private static Random rnd = new Random();
+        public static string RandomStringLower(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz123456789";
+            string result = "";
+            for (int i = 0; i < length; i++)
+            {
+                result += chars[rnd.Next(chars.Length)];
+            }
+            return result;
+        }
+    }
+}
